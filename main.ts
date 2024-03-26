c2191867f4a02985a9ed222f3af6e790b56000c3
SerMaroch <117520156+SerMaroch@users.noreply.github.com>
2024-03-26 16:23:34 +0500

    Обновить pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,55 @@
 StartbitV2.startbit_Init()
-StartbitV2.ultrasonic_init(StartbitV2.startbit_ultrasonicPort.port1)
+if (StartbitV2.startbit_getBatVoltage() < 3.55) {
+    StartbitV2.startbit_setPixelRGB(StartbitLights.Light1, StartbitRGBColors.Red)
+    StartbitV2.startbit_setPixelRGB(StartbitLights.Light2, StartbitRGBColors.Red)
+} else {
+    StartbitV2.startbit_setPixelRGB(StartbitLights.Light1, StartbitRGBColors.Green)
+    StartbitV2.startbit_setPixelRGB(StartbitLights.Light2, StartbitRGBColors.Green)
+}
+StartbitV2.startbit_showLight()
+basic.pause(2000)
+StartbitV2.startbit_clearLight()
+let bnth = 0
+let start = -1
 basic.forever(function () {
-    StartbitV2.setPwmServo(StartbitV2.startbit_servorange.range1, 1, 150, 13)
-    if (StartbitV2.startbit_ultrasonic() > 30) {
-        StartbitV2.startbit_setMotorSpeed(96, 100)
-    } else {
-        StartbitV2.startbit_setMotorSpeed(0, 0)
+    if (start == -1) {
+        if (!(input.pinIsPressed(TouchPin.P1)) && !(input.pinIsPressed(TouchPin.P2))) {
+            basic.pause(3000)
+            start = 1
+            bnth = 0
+        }
+    }
+    while (start == 1) {
+        bnth += 1
+        if (bnth > 7700) {
+            StartbitV2.startbit_setMotorSpeed(0, 0)
+            basic.pause(100)
+            StartbitV2.startbit_setMotorSpeed(-30, 30)
+            basic.pause(200)
+            while (!(input.pinIsPressed(TouchPin.P2))) {
+            	
+            }
+            basic.pause(50)
+            StartbitV2.startbit_setMotorSpeed(0, 0)
+            basic.pause(50)
+            bnth = 0
+        } else {
+            if (!(input.pinIsPressed(TouchPin.P1)) && !(input.pinIsPressed(TouchPin.P2))) {
+                StartbitV2.startbit_setMotorSpeed(20, 20)
+            } else if (input.pinIsPressed(TouchPin.P1) && !(input.pinIsPressed(TouchPin.P2))) {
+                StartbitV2.startbit_setMotorSpeed(32, 0)
+                bnth += 1.5
+                basic.pause(40)
+            } else if (!(input.pinIsPressed(TouchPin.P1)) && input.pinIsPressed(TouchPin.P2)) {
+                StartbitV2.startbit_setMotorSpeed(0, 32)
+                bnth += 1.5
+                basic.pause(40)
+            } else {
+                StartbitV2.startbit_setMotorSpeed(0, 0)
+                start = -1
+                bnth = 0
+                break;
+            }
+        }
     }
 })
